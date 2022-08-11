ddce935f50e0c0bdb7677e92a0718c0b142ad8f7
jjah0001 <55778146+jjah0001@users.noreply.github.com>
2022-08-12 07:34:31 +1000

    Add files via upload

---
+++ movement_source_code.py
@@ -0,0 +1,10 @@
+import YanAPI
+
+#    初始化SDK
+# *  @param robot_ip  参数：机器人IP地址
+# *  @return 无返回值
+
+ip_addr = '160.69.69.103'
+YanAPI.yan_api_init(ip_addr)
+
+#Run 'hostname -I' on Yanshee terminal to retrieve IP Address
