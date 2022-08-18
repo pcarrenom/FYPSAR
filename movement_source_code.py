4cba9e89162dc7f9bb7a40e45dabaacd765c6f2a
jjah0001 <55778146+jjah0001@users.noreply.github.com>
2022-08-18 11:45:49 +1000

    Add files via upload

--- movement_source_code.py
+++ movement_source_code.py
@@ -5,6 +5,16 @@ import YanAPI
 # *  @return 无返回值
 
 ip_addr = '160.69.69.103'
-YanAPI.yan_api_init(ip_addr)
+print(YanAPI.get_motion_list())
 
+def robot_pause():
+    while YanAPI.get_current_motion_play_state()['data']['status'] != "idle":
+        pass
+
+'''
+YanAPI.yan_api_init(ip_addr)
+YanAPI.start_play_motion(name = movement_name, speed = "slow") #Unknown which movements are available
+robot_pause()
+YanAPI.start_play_motion(name = 'reset')
 #Run 'hostname -I' on Yanshee terminal to retrieve IP Address
+'''
