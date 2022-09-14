991a23d17885084c5f66eeea5972146bde44383b
jjah0001 <55778146+jjah0001@users.noreply.github.com>
2022-09-14 14:27:41 +1000

    Used test_subscriber to understand how the rize node communicates

--- rize_integration_file_new/test_subscriber.py
+++ rize_integration_file_new/test_subscriber.py
@@ -3,10 +3,13 @@ import time
 
 # Create a new node
 node = nep.node("subscriber_sample")
-sub = node.new_sub('/blackboard','json')
+#sub_per = node.new_sub('/blackboard','json')
+sub_rize = node.new_sub('rize','json')
 
 while True:
-    s, msg = sub.listen()    # Non-blocking socket
-    print(msg)
-    time.sleep(5)
+    s, msg = sub_rize.listen()    # Non-blocking socket
+    if s == True:
+        print(msg)
+    else:
+        time.sleep(5)
 
